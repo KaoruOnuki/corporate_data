2914277d9dd041d9854e64164ce25deef33f066c
KaoruOnuki <kaoru.onuki@gmail.com>
2019-09-25 11:44:25 +0900

    switch database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_23_214743) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "data", force: :cascade do |t|
     t.string "domain"
     t.string "company"
